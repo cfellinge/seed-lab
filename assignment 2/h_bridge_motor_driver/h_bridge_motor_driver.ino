caa45d4d7d1b5b31e1cacf7620cf3a09a7ce261d
Cody Fellinge <77424437+cfellinge@users.noreply.github.com>
2023-09-13 13:46:02 -0600

    working

--- assignment 2/h_bridge_motor_driver/h_bridge_motor_driver.ino
+++ assignment 2/h_bridge_motor_driver/h_bridge_motor_driver.ino
@@ -29,14 +29,26 @@ void setup() {
   // turn motors on
   digitalWrite(motorTogglePin, HIGH);
 
+  digitalWrite(motorSwitchPin1, HIGH);
+  digitalWrite(motorSwitchPin2, LOW);
+
   Serial.begin(115200);
 }
 
-int x = 100;
-
 void loop() {
-  // put your main code here, to run repeatedly:
-  analogWrite(motorVoltagePin1, x);
-  Serial.print(x);
-  delay(5);
+  // fade in from min to max in increments of 5 points:
+  for (int fadeValue = 0; fadeValue <= 255; fadeValue += 5) {
+    // sets the value (range from 0 to 255):
+    analogWrite(motorVoltagePin1, fadeValue);
+    // wait for 30 milliseconds to see the dimming effect
+    delay(30);
+  }
+
+  // fade out from max to min in increments of 5 points:
+  for (int fadeValue = 255; fadeValue >= 0; fadeValue -= 5) {
+    // sets the value (range from 0 to 255):
+    analogWrite(motorVoltagePin1, fadeValue);
+    // wait for 30 milliseconds to see the dimming effect
+    delay(30);
+  }
 }
